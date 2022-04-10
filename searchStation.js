70f30c826edf08220db7e443ff7846cc03b29eb6
Thomas Off <thomas.off@retiolum.de>
2022-04-11 00:54:46 +0200

    Refine search for station.

--- searchStation.js
+++ searchStation.js
@@ -4,7 +4,9 @@ async function getStations() {
   const spreadsheet = await sheets.spreadsheets.get({
     spreadsheetId: process.env.SPREADSHEET_ID,
   })
-  return spreadsheet.data.sheets.map((sheet) => sheet.properties.title)
+  return spreadsheet.data.sheets
+    .filter((sheet) => sheet.properties.title.startsWith('Bahnhof'))
+    .map((sheet) => sheet.properties.title)
 }
 
 module.exports = async function searchStation(term) {
